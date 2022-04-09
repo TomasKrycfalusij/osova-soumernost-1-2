8ce05dde2bae084c0a9a22f464486d969209d61f
Tomáš Kryčfalušij <91247418+TomasKrycfalusij@users.noreply.github.com>
2022-04-09 06:22:57 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ for i in range(displej):
     led.plot_brightness(a, b, 100)
     b += 1
 
-#TROJUHELNIKY
+#GENEROVANI ZACATKU A SMERU TROJUHELNIKU
 led.set_brightness(255)
 x = 2
 y = randint(0, 4)
@@ -16,3 +16,13 @@ elif y == 1 or 2 or 3:
     smer = randint(1, 3)
 elif y == 4:
     smer = randint(1, 2)
+
+#GVYTVARENI TROJUHELNIKU PODLE SMERU
+if smer == 1:
+    led.plot(x, y)
+    for i in range(2):
+        x -= 1
+        led.plot(x, y)
+    y -= 1
+    x += 1
+    led.plot(x, y)
--- main.ts
+++ main.ts
@@ -1,13 +1,14 @@
+let i: number;
 let smer: number;
 // OSA
 let a = 2
 let b = 0
 let displej = 5
-for (let i = 0; i < displej; i++) {
+for (i = 0; i < displej; i++) {
     led.plotBrightness(a, b, 100)
     b += 1
 }
-// TROJUHELNIKY
+// GENEROVANI ZACATKU A SMERU TROJUHELNIKU
 led.setBrightness(255)
 let x = 2
 let y = randint(0, 4)
@@ -17,5 +18,17 @@ if (y == 0) {
     smer = randint(1, 3)
 } else if (y == 4) {
     smer = randint(1, 2)
+}
+
+// GVYTVARENI TROJUHELNIKU PODLE SMERU
+if (smer == 1) {
+    led.plot(x, y)
+    for (i = 0; i < 2; i++) {
+        x -= 1
+        led.plot(x, y)
+    }
+    y -= 1
+    x += 1
+    led.plot(x, y)
 }
 
